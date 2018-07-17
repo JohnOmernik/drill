e51b1f3c66324b750e12fbffa92a259caa8c4c7b
johnomernik <john@omernik.com>
2018-07-17 10:05:22 -0500

    Trying to get binary to work

--- exec/java-exec/src/main/java/org/apache/drill/exec/store/pcap/PcapRecordReader.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/store/pcap/PcapRecordReader.java
@@ -392,7 +392,7 @@ public class PcapRecordReader extends AbstractRecordReader {
   private void setVarBinaryColumnValue(final byte[] data, final ProjectedColumnInfo pci, final int count) {
     if (data == null) {
       // using .setNull got me an NPE so I am setting the data to [] like Ted did.
-      ByteBuffer value = ByteBuffer.wrap("[]");
+      ByteBuffer value = ByteBuffer.wrap("[]".getBytes());
       ((NullableVarBinaryVector.Mutator) pci.vv.getMutator())
         .setSafe(count, value, 0, value.remaining());
       //((NullableVarBinaryVector.Mutator) pci.vv.getMutator())
